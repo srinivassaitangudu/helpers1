6bcfbf513bdef82cef388ec847b38191e5c8a5c8
GP Saggese <33238329+gpsaggese@users.noreply.github.com>
2024-08-22 10:47:26 -0400

    Sports_analyticsTask1_Build_Docker_container_for_this_repo_1 (#9)
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Improve
    
    * Improve
    
    * Update
    
    * Update

--- tasks.py
+++ tasks.py
@@ -29,7 +29,6 @@ from helpers.lib_tasks import (  # isort: skip # noqa: F401  # pylint: disable=u
     docker_push_dev_image,
     docker_push_prod_candidate_image,
     docker_push_prod_image,
-    docker_release_all,
     docker_release_dev_image,
     docker_release_prod_image,
     docker_rollback_dev_image,  # TODO(gp): -> docker_release_rollback_dev_image
