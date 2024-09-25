1a24307401203b86cb202e1cfb393d28bdea1e2e
Juraj Smeriga <49269742+jsmerix@users.noreply.github.com>
2024-09-25 20:34:17 +0200

    Remove optimize dependent tests (#47)

--- helpers/test/test_versioning.py
+++ helpers/test/test_versioning.py
@@ -18,16 +18,6 @@ class TestVersioning1(hunitest.TestCase):
         code_version = hversio.get_changelog_version(container_dir_name)
         _LOG.debug("code_version=%s", code_version)
 
-    @pytest.mark.skip(reason="TODO(Juraj): HelpersTask21.")
-    def test_get_changelog_version2(self) -> None:
-        """
-        Test `optimizer` version.
-        """
-        amp_path = hgit.get_amp_abs_path()
-        container_dir_name = os.path.join(amp_path, "optimizer")
-        code_version = hversio.get_changelog_version(container_dir_name)
-        _LOG.debug("code_version=%s", code_version)
-
     def test_get_container_version1(self) -> None:
         container_version = hversio._get_container_version()
         _LOG.debug("container_version=%s", container_version)
