0963d3484eface62442cbd92acea54dd5e11839d
Heanh Sok <heanhsok@gmail.com>
2025-01-13 11:57:13 -0500

    checkpoint (#199)
    Co-authored-by: GP Saggese <33238329+gpsaggese@users.noreply.github.com>

--- helpers/create_links.py
+++ helpers/create_links.py
@@ -205,7 +205,13 @@ def _replace_with_links(
             else:
                 link_target = os.path.abspath(src_file)
             os.symlink(link_target, dst_file)
-            os.chmod(dst_file, 0o444)
+            # Make the file read-only in order to prevent accidental 
+            # modifications.
+            # Set file permissions to `0o555` (-r-xr-xr-x): 
+            # - Owner (read, execute)
+            # - Group (read, execute) 
+            # - Others (read, execute)
+            os.chmod(dst_file, 0o555)
             _LOG.info("Created symlink: %s -> %s", dst_file, link_target)
         except Exception as e:
             _LOG.error("Error creating symlink for %s: %s", dst_file, e)
@@ -251,8 +257,12 @@ def _stage_links(symlinks: List[str]) -> None:
             os.remove(link)
             # Copy file to the symlink location.
             shutil.copy2(target_file, link)
-            # Make the file writable.
-            os.chmod(link, 0o644)
+            # Make the file writable to allow for modifications.
+            # Set file permissions to `0o755` (-rwxr-xr-x): 
+            # - Owner (read, write, execute)
+            # - Group (read, execute)
+            # - Others (read, execute)
+            os.chmod(link, 0o755)
             _LOG.info("Staged: %s -> %s", link, target_file)
         except Exception as e:
             _LOG.error("Error staging link %s: %s", link, e)
