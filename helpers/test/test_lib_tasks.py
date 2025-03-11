2917cf41223c54474cca51e261429dded2b0ebae
Heanh Sok <heanhsok@gmail.com>
2025-03-10 21:43:04 -0400

    CmampTask11550_Build_fail___cmamp___Slow_tests___13743543632 (#351)
    * checkpoint
    
    * lint
    
    * checkpoint
    
    ---------
    
    Co-authored-by: Samarth KaPatel <samarth.kapatel5@gmail.com>

--- helpers/test/test_lib_tasks.py
+++ helpers/test/test_lib_tasks.py
@@ -16,6 +16,7 @@ import helpers.hserver as hserver
 import helpers.hsystem as hsystem
 import helpers.hunit_test as hunitest
 import helpers.lib_tasks as hlibtask
+import helpers.lib_tasks_gh as hlitagh
 import helpers.lib_tasks_utils as hlitauti
 
 _LOG = logging.getLogger(__name__)
@@ -33,6 +34,11 @@ def _get_default_params() -> Dict[str, str]:
         "HELPERS_IMAGE_PROD": f"{ecr_base_path}/helpers:prod",
     }
     return default_params
+
+
+# #############################################################################
+# _LibTasksTestCase
+# #############################################################################
 
 
 class _LibTasksTestCase(hunitest.TestCase):
@@ -71,6 +77,11 @@ def _build_mock_context_returning_ok() -> invoke.MockContext:
     return ctx
 
 
+# #############################################################################
+# _CheckDryRunTestCase
+# #############################################################################
+
+
 class _CheckDryRunTestCase(hunitest.TestCase):
     """
     Test class running an invoke target with/without dry-run and checking that
@@ -105,6 +116,11 @@ class _CheckDryRunTestCase(hunitest.TestCase):
 # TODO(gp): We should group the tests by what is tested and not how it's
 # tested. E.g. TestDryRunTasks1::test_print_setup and
 # TestDryRunTasks2::test_print_setup should go together in a class.
+
+
+# #############################################################################
+# TestDryRunTasks1
+# #############################################################################
 
 
 class TestDryRunTasks1(hunitest.TestCase):
@@ -222,6 +238,11 @@ class TestDryRunTasks1(hunitest.TestCase):
 # #############################################################################
 
 
+# #############################################################################
+# TestDryRunTasks2
+# #############################################################################
+
+
 # Outside CK infra, the class hangs, so we skip it.
 @pytest.mark.requires_ck_infra
 @pytest.mark.slow(reason="Around 7s")
@@ -307,8 +328,12 @@ class TestDryRunTasks2(_LibTasksTestCase, _CheckDryRunTestCase):
     def test_gh_create_pr1(self) -> None:
         with umock.patch.object(
             hgit, "get_branch_name", return_value="AmpTask1_test_branch"
+        ), umock.patch.object(
+            hlitagh,
+            "_get_repo_full_name_from_cmd",
+            return_value=("github.com/alphamatic/amp", "amp"),
         ):
-            target = "gh_create_pr(ctx, repo_short_name='amp', title='test')"
+            target = "gh_create_pr(ctx, title='test')"
             self._check_output(target)
 
     # TODO(ShaopengZ): Outside CK infra, the test hangs, so we skip it.
@@ -320,8 +345,12 @@ class TestDryRunTasks2(_LibTasksTestCase, _CheckDryRunTestCase):
     def test_gh_create_pr2(self) -> None:
         with umock.patch.object(
             hgit, "get_branch_name", return_value="AmpTask1_test_branch"
+        ), umock.patch.object(
+            hlitagh,
+            "_get_repo_full_name_from_cmd",
+            return_value=("github.com/alphamatic/amp", "amp"),
         ):
-            target = "gh_create_pr(ctx, body='hello_world', repo_short_name='amp', title='test')"
+            target = "gh_create_pr(ctx, body='hello_world', title='test')"
             self._check_output(target)
 
     # TODO(ShaopengZ): Outside CK infra, the test hangs, so we skip it.
@@ -333,8 +362,12 @@ class TestDryRunTasks2(_LibTasksTestCase, _CheckDryRunTestCase):
     def test_gh_create_pr3(self) -> None:
         with umock.patch.object(
             hgit, "get_branch_name", return_value="AmpTask1_test_branch"
+        ), umock.patch.object(
+            hlitagh,
+            "_get_repo_full_name_from_cmd",
+            return_value=("github.com/alphamatic/amp", "amp"),
         ):
-            target = "gh_create_pr(ctx, draft=False, repo_short_name='amp', title='test')"
+            target = "gh_create_pr(ctx, draft=False, title='test')"
             self._check_output(target)
 
     def test_gh_issue_title(self) -> None:
@@ -372,8 +405,7 @@ class TestDryRunTasks2(_LibTasksTestCase, _CheckDryRunTestCase):
     def test_git_branch_create2(self) -> None:
         # Difference between `cmamp` and `kaizenflow`.
         target = (
-            "git_branch_create(ctx, issue_id=1, repo_short_name='cmamp', "
-            "only_branch_from_master=False)"
+            "git_branch_create(ctx, issue_id=1, only_branch_from_master=False)"
         )
         self._check_output(target)
 
@@ -470,6 +502,11 @@ class TestDryRunTasks2(_LibTasksTestCase, _CheckDryRunTestCase):
 # - lint
 
 
+# #############################################################################
+
+
+# #############################################################################
+# TestFailing
 # #############################################################################
 
 
