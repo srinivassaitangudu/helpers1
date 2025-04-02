7a5b71ecb6ad5ca430c3c97122a86c1bfd95100f
Sonya Nikiforova <son.nik@mail.ru>
2025-04-02 21:36:40 +0200

    HelpersTask471: Generalize to Linux external systems (#480)

--- dev_scripts_helpers/thin_client/thin_client_utils.py
+++ dev_scripts_helpers/thin_client/thin_client_utils.py
@@ -1,3 +1,9 @@
+"""
+Import as:
+
+import dev_scripts_helpers.thin_client.thin_client_utils as dshtctcut
+"""
+
 import argparse
 import logging
 import os
@@ -11,6 +17,7 @@ sys.path.append(
 import helpers.hdbg as hdbg
 import helpers.hparser as hparser
 import helpers.hprint as hprint
+import helpers.hserver as hserver
 import helpers.hsystem as hsystem
 
 _LOG = logging.getLogger(__name__)
@@ -299,15 +306,14 @@ def create_tmux_session(
     _LOG.info("The tmux session doesn't exist, creating it")
     # Make sure we are outside a tmux session.
     dassert_not_inside_tmux()
-    os_name = hsystem.get_os_name()
-    user_name = hsystem.get_user_name()
-    if os_name == "Darwin":
-        _LOG.info("Inferred MacOS setup")
+    if hserver.is_mac() or hserver.is_external_linux():
+        _LOG.info("Inferred external setup")
         home_dir = get_home_dir()
-    elif os_name == "Linux":
+    elif hserver.is_dev_ck():
         _LOG.info("Inferred server setup")
         server_name = hsystem.get_server_name()
         hdbg.dassert_in(server_name, ["dev1", "dev2", "dev3"])
+        user_name = hsystem.get_user_name()
         home_dir = f"/data/{user_name}"
     hdbg.dassert_ne(home_dir, "")
     _LOG.info("home_dir=%s", home_dir)
