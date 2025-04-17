e59ca94fd6f940962a508078bc2f8aa66dad85d5
allenmatt10 <157498336+allenmatt10@users.noreply.github.com>
2025-04-17 06:59:36 -0400

    HelpersTask568_moving_line_in_golden_output (#584)
    * HelpersTask568: Filtered out glitchy line
    
    * Typo fix
    
    * Added todo and skip markers back

--- linters/test/test_amp_dev_scripts.py
+++ linters/test/test_amp_dev_scripts.py
@@ -121,9 +121,12 @@ class Test_linter_py1(hunitest.TestCase):
         file_name = "hello.md"
         as_system_call = True
         output = self.run_linter(txt, file_name, as_system_call)
-        # Remove the line:
-        # '12-16_14:59 ^[[33mWARNING^[[0m: _refresh_toc   :138 : No tags for table'
-        output = hunitest.filter_text("No tags for table", output)
+        # Remove the lines:
+        # '12-16_14:59 ^[[33mWARNING^[[0m: _refresh_toc   :138 : No tags for table'.
+        # '$GIT_ROOT/linters/test/outcomes/.../hello.md: is not referenced in README.md'.
+        log_filters = ["No tags for table", "is not referenced in README.md"]
+        for log_filter in log_filters:
+            output = hunitest.filter_text(log_filter, output)
         # Check.
         self.check_string(output, purify_text=True)
 
@@ -147,9 +150,12 @@ nothing should be changed
         file_name = "hello.md"
         as_system_call = True
         output = self.run_linter(txt, file_name, as_system_call)
-        # Remove the line:
-        # '12-16_14:59 ^[[33mWARNING^[[0m: _refresh_toc   :138 : No tags for table'
-        output = hunitest.filter_text("No tags for table", output)
+        # Remove the lines:
+        # '12-16_14:59 ^[[33mWARNING^[[0m: _refresh_toc   :138 : No tags for table'.
+        # '$GIT_ROOT/linters/test/outcomes/.../hello.md: is not referenced in README.md'.
+        log_filters = ["No tags for table", "is not referenced in README.md"]
+        for log_filter in log_filters:
+            output = hunitest.filter_text(log_filter, output)
         # Check.
         self.check_string(output, purify_text=True)
 
